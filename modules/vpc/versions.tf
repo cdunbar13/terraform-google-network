4469a18f633448200627133d7aec4a7ab6bb9728
Carson Dunbar <carsondunbar@google.com>
2025-04-17 15:52:10 +0000

    Update to VPC module to use google provider - no longer need beta

--- modules/vpc/versions.tf
+++ modules/vpc/versions.tf
@@ -20,7 +20,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.64, < 7"
+      version = ">= 6.19, < 7"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
